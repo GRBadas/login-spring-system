07fde4c9fa311ae991ff2379945eabe61ec3055c
Gabriel Badaró <grbadas@gmail.com>
2023-03-18 23:31:59 -0300

    clean

--- src/main/java/com/badas/todo/repository/TodoReposiroty.java
+++
@@ -1,9 +0,0 @@
-package com.badas.todo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.badas.todo.model.Todo;
-
-public interface TodoReposiroty extends JpaRepository<Todo, Long>{
-
-}
